05da32e8a93595425469a8e2990afd3b85a78372
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-22 20:21:47 -0500

    code to connect app to mysql is created

--- start.js
+++ start.js
@@ -1,2 +1,15 @@
 var inquirer = require('inquirer')
 
+
+const db = require( './app/connection' )('employee_db','rootroot')
+
+
+
+async function showResult(){
+
+ let result;
+ result =  await db.query("select * from tblEmployee;");
+ console.log(result);
+}
+
+showResult()
