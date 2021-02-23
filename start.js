995aec94baffc0be4a2132569668e88730704500
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-22 23:30:47 -0500

    view all employee by role is built

--- start.js
+++ start.js
@@ -56,13 +56,43 @@ for(i=0; i<test.length;i++){
 }
 
 
+async function selectDisplayRole(){
+    let allRoles = []
+    let test = await db.query("select title from tblRole;")
+    for(i=0; i<test.length;i++){
+        allRoles.push(test[i].title)
+    }
+        const role = await inquirer.prompt([
+            {
+                name: "options",
+                type: "list",
+                message: "Select a role",
+                choices: allRoles,
+            }
+        ])
+
+
+    let employeesByRole
+
+    employeesByRole = await db.query(`select E.empID as ID, E.firstName as First_Name, E.lastName as Last_Name, R.title as Title, D.name as Department , R.salary as Salary ,concat(M.firstName, ' ', M.lastName) as Manager 
+    from tblEmployee as E 
+    left join tblEmployee as M 
+    on E.ManagerID = M.empID
+    left join tblRole as R
+    on E.roleID =  R.roleID 
+    left join tblDepartment as D
+    on R.departmentID = D.depID where R.title = "${role.options}"; `)
+    console.log(employeesByRole)
+    main()
+}
+
 async function main(){
     const response = await inquirer.prompt([
         {
             name: "options",
             type: "list",
             message: "what would you like to do?",
-            choices: ["View All Employees","View All Employees By Department", "View All Employees By Manager"]
+            choices: ["View All Employees","View All Employees By Department", "View All Employees By Role","View All Employees By Manager"]
         }
     ])
 
@@ -72,6 +102,9 @@ async function main(){
 
     if(response.options === "View All Employees By Department"){
         selectDisplayDepartment();
+    }
+    if(response.options ==="View All Employees By Role"){
+        selectDisplayRole();   
     }
 }
 
