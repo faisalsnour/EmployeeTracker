3e2240b9247e69749b640ef3627438e0bcbf5083
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-22 23:08:35 -0500

    display employees by department function is built

--- start.js
+++ start.js
@@ -25,6 +25,36 @@ main()
 
 }
 
+async function selectDisplayDepartment(){
+
+let allDepartments = []
+let test = await db.query("select name from tblDepartment;")
+for(i=0; i<test.length;i++){
+   allDepartments.push(test[i].name)
+}
+    const department = await inquirer.prompt([
+        {
+            name: "options",
+            type: "list",
+            message: "Select a department",
+            choices: allDepartments,
+        }
+    ])
+
+    let employeesByDepartment 
+
+    employeesByDepartment = await db.query(`select E.empID as ID, E.firstName as First_Name, E.lastName as Last_Name, R.title as Title, D.name as Department , R.salary as Salary ,concat(M.firstName, ' ', M.lastName) as Manager 
+    from tblEmployee as E 
+    left join tblEmployee as M 
+    on E.ManagerID = M.empID
+    left join tblRole as R
+    on E.roleID =  R.roleID 
+    left join tblDepartment as D
+    on R.departmentID = D.depID where D.name = "${department.options}"; `)
+    console.log(employeesByDepartment);
+    main()
+}
+
 
 async function main(){
     const response = await inquirer.prompt([
@@ -39,6 +69,19 @@ async function main(){
     if(response.options === "View All Employees"){
         viewAllEmployee();
     }
+
+    if(response.options === "View All Employees By Department"){
+        selectDisplayDepartment();
+    }
 }
 
+async function test(){
+let allDepartments = []
+let test = await db.query("select name from tblDepartment;")
+for(i=0; i<test.length;i++){
+   allDepartments.push(test[i].name)
+}
+console.log(allDepartments)
+}
 main()
+// test()
