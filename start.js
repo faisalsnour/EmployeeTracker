1d9122121b6f158ceddc38a09729b8d0adef77fc
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-22 22:06:25 -0500

    query to select all employee is created

--- start.js
+++ start.js
@@ -8,8 +8,20 @@ const db = require( './app/connection' )('employee_db','rootroot')
 async function showResult(){
 
  let result;
- result =  await db.query("select * from tblEmployee;");
- console.log(result);
+//  result =  await db.query("select * from tblEmployee;");
+//  console.log(result);
+
+result = await db.query(`select E.empID as ID, E.firstName as First_Name, E.lastName as Last_Name, R.title as Title, D.name as Department , R.salary as Salary ,concat(M.firstName, ' ', M.lastName) as Manager 
+from tblEmployee as E 
+left join tblEmployee as M 
+on E.ManagerID = M.empID
+left join tblRole as R
+on E.roleID =  R.roleID 
+left join tblDepartment as D
+on R.departmentID = D.depID;`)
+
+console.log(`Result =`, result)
+
 }
 
 showResult()
